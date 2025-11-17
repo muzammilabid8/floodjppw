3dd04b01836d8e0437164bd8357832065432a890
muzammilabid8 <160469238+muzammilabid8@users.noreply.github.com>
2025-11-17 20:31:31 +0500

    Refactor app.py for improved readability

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ import streamlit as st
 df = pd.read_csv("data (1).csv")
 
 st.title("Search Your Record")
+query = st.text_input("Enter Name, CNIC, or Phone")
+search_btn = st.button("Search")
 
 # Message
 st.write("### Pray for Flood Affecties")
 st.write(" This list contains the people from Jalalpur Pirwala whose cards have been received in the camp or the Fund is approved. Missing names will be added daily. If your name appears under “PLRA  Not Verified,” please visit the AC office to check whether your application has been rejected or not.")
 st.write("#### Enter CNIC without hyphen - like(3630111111111) and phone number without 0 like (3000900786)")
-query = st.text_input("Enter Name, CNIC, or Phone")
-search_btn = st.button("Search")
 
 if search_btn:
     if query.strip() == "":
@@ -25,4 +25,5 @@ if search_btn:
             st.write(results)
         else:
             st.error("No record found.")
+
 
