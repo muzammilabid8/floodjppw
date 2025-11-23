11bd3b917f330723a22cf639f16dc6a821f93ff8
muzammilabid8 <160469238+muzammilabid8@users.noreply.github.com>
2025-11-23 16:16:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ github_files = [
     "https://raw.githubusercontent.com/muzammilabid8/floodjppw/main/data21.csv",
     "https://raw.githubusercontent.com/muzammilabid8/floodjppw/main/kin.csv",
     "https://raw.githubusercontent.com/muzammilabid8/floodjppw/main/calls21.csv",
+    "https://raw.githubusercontent.com/muzammilabid8/floodjppw/main/calls23.csv"
 ]
 
 # Load all CSVs into a single DataFrame
@@ -49,6 +50,7 @@ if search_btn:
             st.write(results)
         else:
             st.error("اس وقت کوئی ریکارڈ نہیں ملا۔ انتظار کریں، ان شاء اللہ آپ کا فنڈ منظور ہو جائے گا۔")
+
 
 
 
