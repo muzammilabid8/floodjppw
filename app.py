144628b82dcc58957daa1420e636a9647cb4fed3
muzammilabid8 <160469238+muzammilabid8@users.noreply.github.com>
2025-11-24 07:52:59 +0500

    Add calls24.csv to GitHub files for data loading
    Added a new URL to the GitHub files list for data loading.

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ github_files = [
     "https://raw.githubusercontent.com/muzammilabid8/floodjppw/main/data21.csv",
     "https://raw.githubusercontent.com/muzammilabid8/floodjppw/main/kin.csv",
     "https://raw.githubusercontent.com/muzammilabid8/floodjppw/main/calls21.csv",
-    "https://raw.githubusercontent.com/muzammilabid8/floodjppw/main/calls23.csv"
+    "https://raw.githubusercontent.com/muzammilabid8/floodjppw/main/calls23.csv",
+    "https://raw.githubusercontent.com/muzammilabid8/floodjppw/main/calls24.csv"
 ]
 
 # Load all CSVs into a single DataFrame
@@ -50,6 +51,7 @@ if search_btn:
             st.write(results)
         else:
             st.error("اس وقت کوئی ریکارڈ نہیں ملا۔ انتظار کریں، ان شاء اللہ آپ کا فنڈ منظور ہو جائے گا۔")
+
 
 
 
