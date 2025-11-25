d23b7cd2b3eb7f1068f35db380dae3cd971aecfa
muzammilabid8 <160469238+muzammilabid8@users.noreply.github.com>
2025-11-25 22:13:53 +0500

    Update app.py to include more CSV sources
    Added additional CSV files for data loading and updated search functionality.

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ github_files = [
     "https://raw.githubusercontent.com/muzammilabid8/floodjppw/main/calls21.csv",
     "https://raw.githubusercontent.com/muzammilabid8/floodjppw/main/calls23.csv",
     "https://raw.githubusercontent.com/muzammilabid8/floodjppw/main/calls24.csv",
+    "https://raw.githubusercontent.com/muzammilabid8/floodjppw/main/calls25.csv",
+    "https://raw.githubusercontent.com/muzammilabid8/floodjppw/main/calls26.csv",
+    
 ]
 
 # Load all CSVs into a single DataFrame
@@ -51,6 +54,7 @@ if search_btn:
             st.write(results)
         else:
             st.error("اس وقت کوئی ریکارڈ نہیں ملا۔ انتظار کریں، ان شاء اللہ آپ کا فنڈ منظور ہو جائے گا۔")
+
 
 
 
