afc25fb3dec7fb362c3dff8fd425f183fb770074
muzammilabid8 <160469238+muzammilabid8@users.noreply.github.com>
2025-12-11 13:03:47 +0500

    Refactor app.py for improved readability and structure

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ github_files = [
      "https://raw.githubusercontent.com/muzammilabid8/floodjppw/main/calls02.csv",
     "https://raw.githubusercontent.com/muzammilabid8/floodjppw/main/calls03.csv",
     "https://raw.githubusercontent.com/muzammilabid8/floodjppw/main/master2400.csv",
-    "https://raw.githubusercontent.com/muzammilabid8/floodjppw/main/MASTER24112025.csv",
+    "https://raw.githubusercontent.com/muzammilabid8/floodjppw/main/MASTER2411.csv",
     
 ]
 
@@ -99,6 +99,7 @@ st.write("kin.csv")
 st.write("اور")
 st.write("plra not verified")
 st.write("میں اگر آپ کا نام آ جائے تو اس کا مطلب ہے کہ آپ کی درخواست زمین کی وراثتی مسائل کی وجہ سے مسترد ہو گئی ہے۔ پٹواری سے رابطہ کریں اور وراثت منتقل کریں۔")
+
 
 
 
